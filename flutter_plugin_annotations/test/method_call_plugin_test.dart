baa35d658da2ec12c053b2e7a3ee8e25c6e955c7
Júlio Cotta <juliocbcotta@gmail.com>
2019-08-12 15:03:14 -0300

    removed flutter dependency

--- flutter_plugin_annotations/test/method_call_plugin_test.dart
+++ flutter_plugin_annotations/test/method_call_plugin_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_plugin_annotations/flutter_plugin_annotations.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
+
 
 void main() {
   group('Testing the creation of the MethodCallPlugin class', () {
