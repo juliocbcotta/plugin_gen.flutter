7df1dff0588197541b2a6c1bd0ec16c881ed1267
Júlio Cotta <juliocbcotta@gmail.com>
2019-08-12 01:20:14 -0300

    fixed builder formatting

--- flutter_plugin_generator/lib/builder.dart
+++ flutter_plugin_generator/lib/builder.dart
@@ -4,5 +4,5 @@ import 'package:flutter_plugin_generator/src/flutter_plugin_generator.dart';
 
 /// Builder used to generate plugin classes.
 /// [source_gen] based : https://github.com/dart-lang/source_gen/
-Builder flutterPluginBuilder(BuilderOptions options) =>
-    SharedPartBuilder(const [FlutterPluginGenerator()], 'flutter_plugin_builder');
+Builder flutterPluginBuilder(BuilderOptions options) => SharedPartBuilder(
+    const [FlutterPluginGenerator()], 'flutter_plugin_builder');
