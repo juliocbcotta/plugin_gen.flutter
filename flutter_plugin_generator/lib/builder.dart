a78f6167d09f683245b3f0d79a042325f9d55927
Júlio Cotta <juliocbcotta@gmail.com>
2019-08-12 00:49:42 -0300

    added docs for builder.dart.

--- flutter_plugin_generator/lib/builder.dart
+++ flutter_plugin_generator/lib/builder.dart
@@ -2,6 +2,7 @@ import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:flutter_plugin_generator/src/flutter_plugin_generator.dart';
 
-
+/// Builder used to generate plugin classes.
+/// [source_gen] based : https://github.com/dart-lang/source_gen/
 Builder flutterPluginBuilder(BuilderOptions options) =>
     SharedPartBuilder(const [FlutterPluginGenerator()], 'flutter_plugin_builder');
