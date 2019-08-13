50dd666961df80e7453b978da383add9651787e1
Júlio Cotta <juliocbcotta@gmail.com>
2019-08-13 04:47:09 -0300

    - added EventChannelStream annotation support. - added support to use getters and fields.

--- flutter_plugin_generator/lib/src/flutter_plugin_generator.dart
+++ flutter_plugin_generator/lib/src/flutter_plugin_generator.dart
@@ -76,48 +76,89 @@ class FlutterPluginGenerator extends GeneratorForAnnotation<MethodCallPlugin> {
     return platform.toString().split('.').last;
   }
 
-  String declareMethods(ClassElement element) {
-    final pluginSupportedPlatforms = findSupportedPlatforms(element.metadata);
+  String declareMethods(
+      ClassElement element, List<SupportedPlatform> pluginSupportedPlatforms) {
+    final buffer = StringBuffer();
+
     final methods = element.methods.where((method) {
       return method.isAbstract &&
           method.isPublic &&
           method.returnType.isDartAsyncFuture;
+    }).toList();
+
+    final fields = element.fields
+        .where((field) => field.isPublic && field.type.isDartAsyncFuture)
+        .toList();
+
+    fields.forEach((field) {
+      buffer.write(declareGetter(field, pluginSupportedPlatforms));
     });
-    final buffer = StringBuffer();
+
     methods.forEach((method) {
-      final methodSupportedPlatforms = findSupportedPlatforms(method.metadata);
-
-      final methodName = method.displayName;
-
-      final methodParams = declareParams(method.parameters);
-      final methodReturnType = method.returnType.displayName;
-
-      buffer.writeln('@override');
-      buffer.writeln('$methodReturnType $methodName($methodParams) async {');
-
-      pluginSupportedPlatforms.forEach((platform) {
-        if (!methodSupportedPlatforms.contains(platform)) {
-          final name = platformName(platform);
-          buffer.writeln('''
-        if (Platform.is$name)
-            throw UnsupportedError('Functionality $methodName is not available on $name.');
-        ''');
-        }
-      });
-
-      final innerType =
-          (method.returnType as ParameterizedType).typeArguments[0];
-      final invokeMethod = selectInvokeMethod(innerType);
-      final invokeParams = selectParamToInvokeMethod(method.parameters);
-      final separator = invokeParams.isEmpty ? '' : ',';
-
-      final resultMapped = mapResultToDart(innerType);
-
-      buffer.writeln(
-          'final result = await _methodChannel.$invokeMethod(\'$methodName\'$separator $invokeParams);');
-      buffer.writeln('$resultMapped');
-      buffer.writeln('}');
+      buffer.write(declareMethod(method, pluginSupportedPlatforms));
     });
+
+    return buffer.toString();
+  }
+
+  String declareGetter(
+    FieldElement field,
+    List<SupportedPlatform> pluginSupportedPlatforms,
+  ) {
+    final buffer = StringBuffer();
+    final methodSupportedPlatforms =
+        findSupportedPlatforms(field.metadata + field.getter.metadata);
+
+    final fieldName = field.displayName;
+
+    final fieldType = field.type.displayName;
+    buffer.writeln('@override');
+    buffer.writeln('$fieldType get $fieldName async {');
+
+    buffer.writeln(processSupportedPlatforms(
+        fieldName, pluginSupportedPlatforms, methodSupportedPlatforms));
+
+    final innerType = (field.type as ParameterizedType).typeArguments[0];
+    final invokeMethod = selectInvokeMethod(innerType);
+
+    final resultMapped = mapResultToDart(innerType);
+
+    buffer.writeln(
+        'final result = await _methodChannel.$invokeMethod(\'$fieldName\');');
+    buffer.writeln('$resultMapped');
+    buffer.writeln('}');
+
+    return buffer.toString();
+  }
+
+  String declareMethod(
+    MethodElement method,
+    List<SupportedPlatform> pluginSupportedPlatforms,
+  ) {
+    final buffer = StringBuffer();
+    final methodSupportedPlatforms = findSupportedPlatforms(method.metadata);
+
+    final methodName = method.displayName;
+
+    final methodParams = declareParams(method.parameters);
+    final methodReturnType = method.returnType.displayName;
+    buffer.writeln('@override');
+    buffer.writeln('$methodReturnType $methodName($methodParams) async {');
+
+    buffer.writeln(processSupportedPlatforms(
+        methodName, pluginSupportedPlatforms, methodSupportedPlatforms));
+
+    final innerType = (method.returnType as ParameterizedType).typeArguments[0];
+    final invokeMethod = selectInvokeMethod(innerType);
+    final invokeParams = selectParamToInvokeMethod(method.parameters);
+    final separator = invokeParams.isEmpty ? '' : ',';
+
+    final resultMapped = mapResultToDart(innerType);
+
+    buffer.writeln(
+        'final result = await _methodChannel.$invokeMethod(\'$methodName\'$separator $invokeParams);');
+    buffer.writeln('$resultMapped');
+    buffer.writeln('}');
 
     return buffer.toString();
   }
@@ -233,7 +274,7 @@ class FlutterPluginGenerator extends GeneratorForAnnotation<MethodCallPlugin> {
   }
 
   String selectParamToInvokeMethod(List<ParameterElement> params) {
-    if (params.length == 0) {
+    if (params.isEmpty) {
       return '';
     } else if (params.length == 1) {
       final param = params[0];
@@ -297,9 +338,71 @@ class FlutterPluginGenerator extends GeneratorForAnnotation<MethodCallPlugin> {
     }
   }
 
+  String findStreamChannelName(List<ElementAnnotation> metadata) {
+    final annotation = metadata.firstWhere(
+        (annotation) =>
+            annotation.computeConstantValue().type.displayName ==
+            'EventChannelStream',
+        orElse: () => null);
+
+    return annotation == null
+        ? null
+        : annotation.constantValue.getField('channelName').toStringValue();
+  }
+
+  bool isDartStream(DartType type) {
+    return type.displayName.startsWith('Stream<');
+  }
+
   String generatePluginTemplate(Element element, ConstantReader annotation) {
     final channelName = annotation.read('channelName').stringValue;
     final className = element.displayName;
+
+    final factory = createFactory(element, className, channelName);
+
+    final pluginSupportedPlatforms = findSupportedPlatforms(element.metadata);
+
+    final streamGetters = (element as ClassElement)
+        .fields
+        .where((field) => isDartStream(field.type))
+        .map((field) {
+      final channelName =
+          findStreamChannelName(field.metadata + field.getter.metadata);
+      final innerType = (field.type as ParameterizedType).typeArguments[0];
+      final result = mapResultToDart(innerType);
+      final methodSupportedPlatforms =
+          findSupportedPlatforms(field.metadata + field.getter.metadata);
+      final platformOnly = processSupportedPlatforms(field.displayName,
+          pluginSupportedPlatforms, methodSupportedPlatforms);
+      return '''
+        static const EventChannel _${field.displayName}EventChannel = const EventChannel('$channelName');
+        
+        final _${field.displayName} = _${field.displayName}EventChannel.receiveBroadcastStream();
+        
+        @override
+        ${field.type.displayName} get ${field.displayName} {
+            $platformOnly
+        return  _${field.displayName}.map((result){  $result });
+        }
+      ''';
+    }).join('\n');
+
+    return '''
+    
+    class _\$$className extends $className {
+    
+    
+      $factory
+    
+      $streamGetters
+    
+      ${declareMethods(element, pluginSupportedPlatforms)}
+    
+    }  
+    ''';
+  }
+
+  String createFactory(Element element, String className, String channelName) {
     final pathRegexp = RegExp('(\{.*?\})');
     final groups =
         pathRegexp.allMatches(channelName).map((match) => match.group(0));
@@ -334,17 +437,24 @@ class FlutterPluginGenerator extends GeneratorForAnnotation<MethodCallPlugin> {
         
         _\$$className();
       ''';
+    return factory;
+  }
 
-    return '''
-    
-    class _\$$className extends $className {
-    
-    
-      $factory
-    
-      ${declareMethods(element)}
-    
-    }  
-    ''';
+  String processSupportedPlatforms(
+      String elementName,
+      List<SupportedPlatform> pluginSupportedPlatforms,
+      List<SupportedPlatform> methodSupportedPlatforms) {
+    final buffer = StringBuffer();
+
+    pluginSupportedPlatforms.forEach((platform) {
+      if (!methodSupportedPlatforms.contains(platform)) {
+        final name = platformName(platform);
+        buffer.writeln('''
+        if (Platform.is$name)
+            throw UnsupportedError('Functionality $elementName is not available on $name.');
+        ''');
+      }
+    });
+    return buffer.toString();
   }
 }
