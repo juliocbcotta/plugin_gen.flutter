0b9f07307c954ac58b2a65e2994bcbabe40177fb
Júlio Cotta <juliocbcotta@gmail.com>
2019-08-14 09:58:51 -0300

    updated test.

--- flutter_plugin_annotations/test/method_call_plugin_test.dart
+++ flutter_plugin_annotations/test/method_call_plugin_test.dart
@@ -2,9 +2,9 @@ import 'package:flutter_plugin_annotations/flutter_plugin_annotations.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Testing the creation of the MethodCallPlugin class', () {
-    test('Creating an MethodCallPlugin with channelNamel', () {
-      final plugin = MethodCallPlugin(channelName: 'some name');
+  group('Testing the creation of the MethodChannelFutures class', () {
+    test('Creating an MethodChannelFutures with channelNamel', () {
+      final plugin = MethodChannelFutures(channelName: 'some name');
       expect(plugin.channelName, 'some name');
     });
   });
